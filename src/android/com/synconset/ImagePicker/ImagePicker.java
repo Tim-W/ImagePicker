beb2071329a4e3807d0d09c526cf891e24d17d44
Tim Wissel <twtimwissel5@gmail.com>
2017-01-16 13:44:41 +0100

    test4

--- src/android/com/synconset/ImagePicker/ImagePicker.java
+++ src/android/com/synconset/ImagePicker/ImagePicker.java
@@ -21,6 +21,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.content.ClipData;
 
 public class ImagePicker extends CordovaPlugin {
 
